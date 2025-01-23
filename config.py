891265d1977ae8b93db01a46d6da90a153ff2745
slayer123700 <meerutuniversity996@gmail.com>
2025-01-23 16:21:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/itzshukla/PRINCE-MUSIC",
+    "https://github.com/slayer123700/test-repo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
